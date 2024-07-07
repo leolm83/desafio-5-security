9486d57b8ce0bc0f42eefa48bfac2ca72fdb10da
leolm83 <leonardo.mateus01@hotmail.com>
2024-07-07 09:40:51 -0300

    user,role e seed

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -1,15 +1,18 @@
 package com.devsuperior.dscommerce.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,9 +25,12 @@ public class User {
     private LocalDate birthDate;
     private String password;
 
-    @OneToMany(mappedBy = "client")
-    private List<Order> orders = new ArrayList<>();
 
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
     public User() {
     }
 
@@ -60,7 +66,6 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getPhone() {
         return phone;
     }
@@ -77,18 +82,56 @@ public class User {
         this.birthDate = birthDate;
     }
 
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public void setPassword(String password) {
         this.password = password;
     }
-
-    public List<Order> getOrders() {
-        return orders;
+    public void addRole(Role role){
+        roles.add(role);
     }
-
+    public boolean hasRole(String roleName){
+        if(Objects.isNull(roleName)) return false;
+        for(Role role : roles){
+        if(roleName.equals(role.getAuthority())){
+            return true;
+        }
+       }
+       return false;
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -103,4 +146,5 @@ public class User {
     public int hashCode() {
         return id != null ? id.hashCode() : 0;
     }
+
 }
